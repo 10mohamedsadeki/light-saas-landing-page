ed3a60d5bab49dc530b553fd0740e7ac1edadd7f
10mohamedsadeki <a.sadeki.m@gmail.com>
2025-08-13 15:26:14 +0100

    Fix missing key props for Vercel build

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,33 @@
+import logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import SocialX from "@/assets/social-x.svg"
+import Instagram from "@/assets/social-insta.svg"
+import Linkedin from "@/assets/social-linkedin.svg"
+import Pins from "@/assets/social-pin.svg"
+import Youtube from "@/assets/social-youtube.svg"
 export const Footer = () => {
-  return null;
+  return (
+    <footer id="footer" className="bg-black text-[#BCBCBC] text-sm py-10 text-center">
+      <div className="container">
+        <div className="inline-flex relative">
+        <Image src={logo} alt="Saas Logo" width={40} height={40} />
+        </div>
+        <nav className="flex flex-col md:flex-row md:justify-center gap-6 mt-6"> 
+          <a href="#">About</a>
+          <a href="#">Features</a>
+          <a href="#">Customers</a>
+          <a href="#">Updates</a>
+          <a href="#">Help</a>
+        </nav>
+        <div className="flex justify-center gap-6 mt-6">
+          <SocialX />
+          <Instagram />
+          <Linkedin />
+          <Pins />
+          <Youtube />
+        </div>
+        <p className="mt-6">&copy; 2024 Your Company, Inc. All rights reserved M's.</p>
+      </div>
+    </footer>
+  );
 };
--- src/sections/Pricing.tsx
+++ src/sections/Pricing.tsx
@@ -1,3 +1,5 @@
+import CheckIcon from "@/assets/check.svg";
+import { twMerge } from "tailwind-merge";
 const pricingTiers = [
   {
     title: "Free",
@@ -51,5 +53,86 @@ const pricingTiers = [
 ];
 
 export const Pricing = () => {
-  return null;
+  return (
+    <section className="py-24 bg-white">
+      <div className="container">
+        <div className="max-w-[540px] mx-auto">
+        <h2 className="section-title  text-gradient">Pricing</h2>
+        <p className="section-desc mt-5">
+          Free forever. Upgrade for unlimited tasks, better security, and
+          exclusive features.
+        </p>
+        </div>
+        <div className="flex flex-col gap-6 items-center mt-10 lg:flex-row lg:items-end lg:justify-center">
+          {pricingTiers.map(
+            ({
+              title,
+              monthlyPrice,
+              buttonText,
+              popular,
+              inverse,
+              features,
+            }) => (
+              
+              <div
+                className={twMerge(
+                  "p-10 border border-[#F1F1F1] rounded-3xl shadow-[0_7px_14px_#EAEAEA] max-w-sm w-full",
+                  inverse === true && "border-black bg-black text-white"
+                )}
+                key={title}
+              >
+                <div className="flex justify-between">
+                  <h3
+                    className={twMerge(
+                      "text-lg font-bold text-[#000000]/50",
+                      inverse === true && "text-white/60"
+                    )}
+                  >
+                    {title}
+                  </h3>
+                  {popular === true && (
+                    <div className="inline-flex text-sm px-4 py-1.5 border border-white/20 rounded-lg ">
+                      <span className="bg-[linear-gradient(to_right,#DD7DFF,#E1CD86,#8BCB92,#71C2EF,#3BFFFF,#DD7DFF)] bg-clip-text text-transparent font-medium webkit">
+                        Most Popular
+                      </span>
+                    </div>
+                  )}
+                </div>
+                <div className="flex items-baseline gap-1 mt-[30px]">
+                  <span className="text-4xl font-bold tracking-tighter leading-none">
+                    ${monthlyPrice}
+                  </span>
+                  <span
+                    className={twMerge(
+                      "font-bold tracking-tight text-[#000000]/50",
+                      inverse === true && "text-white/50"
+                    )}
+                  >
+                    /month
+                  </span>
+                </div>
+                <button
+                  className={twMerge(
+                    "btn btn-primary w-full mt-[30px]",
+                    inverse === true && "bg-white text-black"
+                  )}
+                >
+                  {buttonText}
+                </button>
+                <ul className="flex flex-col gap-5 mt-8">
+                  {features.map((feature) => (
+                    
+                    <li key={feature} className="text-sm flex items-center gap-4">
+                      <CheckIcon className="h-6 w-6" />
+                      <span>{feature}</span>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
